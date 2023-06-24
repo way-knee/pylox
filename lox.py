10e0596d4253ac3e41e104e78b6812053a4bc0a8
way-knee <winnie.chen.chouchou@gmail.com>
2023-06-24 10:23:47 -0400

    add additional class def to Lox

--- lox.py
+++ lox.py
@@ -4,5 +4,31 @@ class Lox:
     def __init__(self):
         self.args = sys.argv
         self.interpreter = Interpreter()
+        self.had_error = False
+        self.had_runtime_error = False
 
     def main(self):
+
+    def run_file(self, path: str) -> None:
+        with open(path, 'r') as file:
+           run(file.read())
+        if had_error:
+            sys.exit(65)
+        if had_runtime_error:
+            sys.exit(70)
+
+    def run_prompt(self) -> None:
+        while true:
+            line = input('> ')
+            if line == None: # to handle Control-C or Control-D
+                break
+            run(line)
+            self.had_error = False
+    
+    def run(source: str) -> None:
+        scanner = Scanner(source)
+        tokens = scanner.scanTokens()
+        for token in tokens:
+            print(token)
+
+
