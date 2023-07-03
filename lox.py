9b89e8f4d447aba0362ecc28ee640bad45531be8
way-knee <winnie.chen.chouchou@gmail.com>
2023-07-03 08:59:01 -0400

    remove Lox class in lox.py

--- lox.py
+++ lox.py
@@ -1,46 +1,36 @@
 import sys
+from scanner import Scanner
 
-class Lox:
-    def __init__(self):
-        self.args = sys.argv
-        self.interpreter = Interpreter()
-        self.had_error = False
-        self.had_runtime_error = False
+args = sys.argv
+had_error = False
+had_runtime_error = False
 
-    def main(self):
-        return
+def run_file(path: str) -> None:
+    with open(path, 'r') as file:
+        run(file.read())
+    if had_error:
+        sys.exit(65)
+    if had_runtime_error:
+        sys.exit(70)
 
-    def run_file(self, path: str) -> None:
-        with open(path, 'r') as file:
-           run(file.read())
-        if self.had_error:
-            sys.exit(65)
-        if self.had_runtime_error:
-            sys.exit(70)
+def run_prompt() -> None:
+    while True:
+        line = input('> ')
+        if line == None: # to handle Control-C or Control-D, do I need this line?
+            break
+        run(line)
+        had_error = False
 
-    def run_prompt(self) -> None:
-        while true:
-            line = input('> ')
-            if line == None: # to handle Control-C or Control-D
-                break
-            self.run(line)
-            self.had_error = False
-    
-    def run(self, source: str) -> None:
-        scanner = Scanner(source)
-        tokens = scanner.scan_tokens()
-        for token in tokens:
-            print(token)
+def run(source: str) -> None:
+    scanner = Scanner(source)
+    tokens = scanner.scan_tokens()
+    for token in tokens:
+        print(token)
 
-    def error(line: int, message: str) -> None:
-        self.report(line, '', message)
+def error(line: int, message: str) -> None:
+    report(line, '', message)
 
-    def report(self, line: int, where: str, message: str) -> None:
-        print(f'[line {line}] Error{where}: {message}')
-        self.had_error = True
-
-
-
-
-
+def report(line: int, where: str, message: str) -> None:
+    print(f'[line {line}] Error{where}: {message}')
+    had_error = True
 
