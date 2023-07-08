ab6fab6da059706ae8aae0c3383576b33a771b84
way-knee <winnie.chen.chouchou@gmail.com>
2023-07-08 17:12:32 -0400

    add factor, unary, and primary methods

--- interpreter/parser.py
+++ interpreter/parser.py
@@ -9,10 +9,10 @@ class Parser():
         self.tokens = tokens
         self.current = 0
 
-    def expression() -> Expr:
+    def expression():
         return self.equality()
 
-    def equality() -> Expr:
+    def equality():
         expr = self.comparison();
         while self.match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL):
             operator = self.previous()
@@ -20,7 +20,7 @@ class Parser():
             expr = Binary(expr, operator, right)
         return expr
 
-    def comparison() -> Expr:
+    def comparison():
         expr = self.term()
         while self.match(TokenType.GREATER,
                          TokenType.GREATER_EQUAL,
@@ -31,13 +31,39 @@ class Parser():
             expr = Binary(expr, operator, right)
         return expr
 
-    def term() -> Expr:
+    def term():
         expr = self.factor()
         while self.match(TokenType.MINUS, TokenType.PLUS):
             operator = self.previous()
             right = self.factor()
             expr = Binary(expr, operator, right)
             return expr
+
+    def factor():
+        expr = self.unary()
+        while self.match(TokenType.SLASH, TokenType.STAR):
+            operator = self.previous()
+            right = self.unary()
+            expr = Binary(expr, operator, right)
+        return expr
+
+    def unary():
+        if self.match(TokenType.BANG, TokenType.MINUS):
+            operator = self.previous()
+            right = self.unary()
+            return Unary(operator, right)
+        return self.primary()
+
+    def primary():
+        if self.match(TokenType.FALSE): return Literal(False)
+        if self.match(TokenType.TRUE): return Literal(True)
+        if self.match(TokenType.NIL): return Literal(None)
+        if self.match(TokenType.NUMBER, TokenType.STRING):
+            return Literal(self.previous().literal)
+        if self.match(TokenType.LEFT_PAREN):
+            expr = self.expression()
+            self.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression")
+            return Grouping(expr)
 
     def match(*types: TokenType):
         for t in types:
